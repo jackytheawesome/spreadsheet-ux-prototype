5d74b72a4a2f64d02da2ed2b58670366f2ff21f6
jackytheawesome <jackytheawesome@gmail.com>
2026-02-21 11:57:31 +0300

    Remove unused dependencies from package-lock.json to streamline the project and reduce bundle size.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from 'react'
-import { Button } from '@gravity-ui/uikit'
 import { createUniver, LocaleType, mergeLocales } from '@univerjs/presets'
 import { UniverSheetsCorePreset } from '@univerjs/preset-sheets-core'
 import UniverPresetSheetsCoreEnUS from '@univerjs/preset-sheets-core/locales/en-US'
@@ -35,10 +34,7 @@ function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <h1 className="app-title">Таблицы (Univer + Gravity UI)</h1>
-        <Button view="action" size="m">
-          Gravity UI
-        </Button>
+        <h1 className="app-title">Таблицы</h1>
       </header>
       <div id="univer-container" className="univer-container" />
     </div>
